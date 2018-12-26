3767b5d9562581bd6d641586653cbbd9979d7bc4
Aiden-Wei <45988164+Aiden-Wei@users.noreply.github.com>
2018-12-26 18:22:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -222,10 +222,8 @@ namespace startbit {
 		else {
 			//actiongroup_finished = true;
                 }
+		actiongroup_finished = true;    
             }
-	    else {
-		    actiongroup_finished = true;
-		 }
             //if (cmd.charAt(0).compare("W") == 0) {
 		//actiongroup_finished = true;  
 	   // }
